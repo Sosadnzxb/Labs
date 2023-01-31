ffc9f0c7da92f0e5af1b3d8d41a990db1b6e82b0
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2023-02-01 02:49:58 +0300

    Update Source.c

--- Laba 6/Laba 6.1/Source.c
+++ Laba 6/Laba 6.1/Source.c
@@ -1,1 +1,18 @@
+#include"arrays.h"
+
+int main()
+{
+	int size = 0, choice = 0, start = 0, end = 0, start1 = 0, end1 = 0;
+	size = array_size_input(size);
+	int* array = calloc(size, sizeof(int));
+	choice = choice_0_or_1(choice);
+	if (choice == 0) fill_your_array(array, size);
+	else fill_random_array(array, size);
+	print_array(array, size);
+	int first = 0, last = size - 1;
+	Hoar_sort_and_time(array,size,first,last,start,end);
+	Bouble_sort_time(array,size, start1,end1);
+}
+
+
 
