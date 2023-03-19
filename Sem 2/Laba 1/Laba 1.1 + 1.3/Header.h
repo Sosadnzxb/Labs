f3c8802c2d3c30b81bc2605c070c112c19099b63
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2023-03-19 06:22:15 +0300

    Create Header.h

---
+++ Sem 2/Laba 1/Laba 1.1 + 1.3/Header.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_numbers(char** argv, int argc);
+
+void print_file(char** argv);
+
+int check_input();
+
+void swap_min_max_nums(char** argv);
+
