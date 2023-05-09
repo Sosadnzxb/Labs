d41cda15cdd4e915b4169cd089d91b51ab56de94
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2023-05-09 21:29:44 +0300

    Update Source.c

--- Sem 2/Laba 2/1.1 + 1.2/Source.c
+++ Sem 2/Laba 2/1.1 + 1.2/Source.c
@@ -1,1 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
 
+#include "Header.h"
+
+int main()
+{
+    FILE* fptr;
+    int num;
+    printf("Enter number of elements: ");
+    while (scanf("%d", &num) != 1 || num < 0 || getchar() != '\n')
+    {
+        printf("Incorrect input. Try again: ");
+        rewind(stdin);
+    }
+    write_file(num);
+    print_file(num);
+    find_max_nums();
+    return 0;
+}
