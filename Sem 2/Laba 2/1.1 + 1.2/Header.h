3e83b9e5226013c03be8e2df4670b3551b19347a
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2023-05-09 21:30:00 +0300

    Update Header.h

--- Sem 2/Laba 2/1.1 + 1.2/Header.h
+++ Sem 2/Laba 2/1.1 + 1.2/Header.h
@@ -1,1 +1,14 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_file(int num);
+
+void print_file(int num);
+
+void find_max_nums();
 
