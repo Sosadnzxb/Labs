4b3546e99e0e3f6dbf109795f9f80518cd36d7e0
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2023-05-11 02:02:01 +0300

    Create Functions.c

---
+++ Sem 2/Laba 5/Functions.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Header.h"
+
+void initialize(Stack* stack) {
+    stack->top = -1;
+}
+
+int isEmpty(Stack* stack) {
+    return stack->top == -1;
+}
+
+int isFull(Stack* stack) {
+    return stack->top == MAX_SIZE - 1;
+}
+
+void push(Stack* stack, int element) {
+    if (isFull(stack)) {
+        printf("Error: Stack is full\n");
+        return;
+    }
+    stack->data[++stack->top] = element;
+}
+
+int pop(Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Error: Stack is empty\n");
+        return -1;
+    }
+    return stack->data[stack->top--];
+}
+
+int getSumOfEvenElements(Stack* stack) {
+    int sum = 0;
+    for (int i = 0; i <= stack->top; i++) {
+        if (stack->data[i] % 2 == 0) {
+            sum += stack->data[i];
+        }
+    }
+    return sum;
+}
