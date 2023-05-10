535a7fbd3fc3f6fea0f0c23c5538e3f4cb0aa122
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2023-05-11 02:03:12 +0300

    Create Source.c

---
+++ Sem 2/Laba 6/Source.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Header.h"
+
+int main() {
+    int maxSize;
+    printf("Enter the maximum queue size: ");
+    while (scanf("%d", &maxSize) != 1 || maxSize <= 0) {
+        printf("Invalid input. Please enter an integer: ");
+        while (getchar() != '\n'); // Очищаем буфер ввода
+    }
+
+    Queue queue = createQueue(maxSize);
+
+    printf("Enter symbols:\n");
+    char input;
+    while (scanf(" %c", &input) == 1) {
+        if (!isEmpty(&queue) && input == queue.rear->data) {
+            printf("Coincidence! Extracted symbol: %c\n", peek(&queue));
+        }
+        enqueue(&queue, input);
+
+        if (queue.size >= queue.maxSize) {
+            printf("Queue is full. Cannot enqueue more elements.\n");
+            break;
+        }
+    }
+
+    printf("Queue after entering symbols:\n");
+    printQueue(&queue);
+
+    return 0;
+}
