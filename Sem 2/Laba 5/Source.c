1e4c1ebcecdcb0690b1531eb6b10c8acb3f54b28
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2023-05-11 02:01:24 +0300

    Create Source.c

---
+++ Sem 2/Laba 5/Source.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Header.h"
+
+
+int main() {
+    Stack stack;                                        //LIFO - Last In First Out
+    initialize(&stack);
+
+    int maxSize;
+    printf("Enter the maximum stack size (1-100): ");
+    while (scanf("%d", &maxSize) != 1 || maxSize < 1 || maxSize > 100) {
+        printf("Invalid input. Please enter an integer from 1 to 100: ");
+        while (getchar() != '\n'); // Очищаем буфер ввода
+    }
+
+    int num;
+    printf("Enter the stack elements:\n");
+    for (int i = 0; i < maxSize; i++) {
+        printf("Element : ", i + 1);
+        while (scanf("%d", &num) != 1) {
+            printf("\nInvalid input. Please enter an integer: ");
+            while (getchar() != '\n'); // Очищаем буфер ввода
+        }
+        push(&stack, num);
+    }
+
+    int sum = getSumOfEvenElements(&stack);
+    printf("Sum of even stack elements: %d\n", sum);
+
+    return 0;
+}
